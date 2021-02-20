7a5d29f0ea3b3455291e34e0adcfde126f2f3eca
cscharf91 <cscharf91@gmail.com>
2021-02-20 12:23:40 -0500

    fixed login page

--- src/modules/auth/Login.js
+++ src/modules/auth/Login.js
@@ -1,26 +1,34 @@
 import React, { useState } from "react";
 import Axios from 'axios';
+import { useHistory } from "react-router-dom";
 
 function Login(props) {
   const [login, setLogin] = useState({ email: "", password: "" });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
   const { user, setUser, setToken } = props;
+  const history = useHistory();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
     try {
-      const { data } = await Axios.post('https://vast-ravine-96250.herokuapp.com/api/auth/login', login);
+      const { data } = await Axios.post('https://thawing-mountain-86418.herokuapp.com/api/auth/login', login);
       const token = {
         headers: {
           'auth-token': data.token
         }
       }
+      setLoading(false);
+      setError("");
       setToken(token);
       setUser(data.user);
       localStorage.setItem('user', JSON.stringify(data.user));
       localStorage.setItem('token', JSON.stringify(token));
-      window.location = '/';
+      history.push('/');
     } catch (err) {
-      console.log(err);
+      setLoading(false);
+      setError("Invalid credentials. Please try again.");
     }
   }
 
@@ -35,6 +43,7 @@ function Login(props) {
   <div className="card">
       {!user &&
       <form onSubmit={handleSubmit}>
+        <p className="danger-txt">{error}</p>
         <div>
           <label>Email:</label>
           <input 
@@ -53,7 +62,7 @@ function Login(props) {
             onChange={handleChange}
           />
         </div>
-        <button type="submit">Submit</button>
+        <button disabled={loading} type="submit">{loading ? "Loading..." : "Submit"}</button>
     </form>}
     {user &&
     <p>You are already logged in.</p>
